c06d34db5e05783c684479e7fc34d202c90089ee
Szymon Misztal <sz.misztal@gmail.com>
2023-04-05 12:20:16 +0200

    add main.py, templates folder, home page screen.png to repo

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    movies = []
+    for i in range(4):
+        movies.append(i)
+    return render_template("homepage.html", movies = movies)
+
+if __name__ == '__main__':
+    app.run(debug = True)
